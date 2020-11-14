8fbac2a7f416cec50ff3897d5aa79993eb69aca1
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-11-14 21:25:05 +0100

    ng-model, ng-click, adding functions to controller using scope.

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,3 +1,6 @@
-app.controller("mainController", ['$scope', function($scope) {
-  
+app.controller("MainController", ['$scope', function($scope) {
+  $scope.list = ['Learn AngularJS', "Play PS4", "Watch Sopranos."];
+  $scope.addItem = function() {
+    $scope.list.push($scope.addTodo);
+  }
 }]);
