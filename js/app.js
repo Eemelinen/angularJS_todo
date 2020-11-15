d0d067f7876512ab411b21c05ae56f7c9d3dc662
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-11-15 20:54:12 +0100

    routing added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('todoList', []);
+var app = angular.module('todoList', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl: "views/todo.html",
+  })
+
+  .when('/courses', {
+    templateUrl: 'views/courses.html'
+  })
+
+  .otherwise({ redirectTo: '/' }); 
+
+})
---
+++ js/controllers/todoController.js
@@ -0,0 +1,18 @@
+app.controller("TodoController", function($scope) {
+  $scope.title = "Todo:";
+
+  // $scope.todo = {
+  //   title: "Things to do",
+  //   list: ["Learn AngularJS", "Play PS4", "Watch Sopranos."],
+  // }
+
+  // $scope.courses = {
+  //   title: "Courses to take",
+  //   list: ["Angular", "PHP", "React"],
+  // };
+
+  // $scope.addItem = function(subject, item) {
+  //   subject.push(item);
+  // }
+
+});
